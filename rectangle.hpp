b3d48aece28c37781ea89b362887f8aea8a68dfa
Bruno Lima Soares <112128273+BrunoL28@users.noreply.github.com>
2022-11-19 16:02:02 -0300

    Subindo rectangle.cpp

---
+++ rectangle.hpp
@@ -0,0 +1,12 @@
+#include <string>
+#include "shape.hpp"
+
+class Rectangle: public Shape{
+    private:
+    int length;
+    int width;
+
+    public:
+    double getArea() override;
+    std::string toString() override;
+};
