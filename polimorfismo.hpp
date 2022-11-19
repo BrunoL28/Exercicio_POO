668bd6510f16a1d6d89e15ca229976ccb540adbb
Bruno Lima Soares <112128273+BrunoL28@users.noreply.github.com>
2022-11-19 15:57:06 -0300

    Subindo polimorfismo.cpp

---
+++ polimorfismo.hpp
@@ -0,0 +1,46 @@
+#ifndef POLIMORFISMO_H
+#define POLIMORFISMO_H
+
+#include <string>
+
+class Circle{
+    
+    private:
+
+        float _radius;
+
+    public:
+
+        Circle(float radius);
+
+        virtual float getRadius();
+
+        virtual float getArea();
+        
+        virtual std::string toString();
+
+        virtual void imprime(Circle *c);
+};
+
+class Cylinder : public Circle{
+
+    private:
+
+        float _height;
+
+    public:
+
+        Cylinder(float r, float h);
+
+        float getHeight();
+
+        float getArea() override;
+
+        float getVolume();
+
+        std::string toString() override;
+
+        void imprime(Cylinder *c);
+};
+
+#endif
